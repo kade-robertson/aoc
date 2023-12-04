a2c31dd9d51ebf5418dbfcd765b2a3ce3c47ab03
Kade Robertson <kade@kaderobertson.dev>
2023-12-04 08:06:00 -0500

    chore: update day 3 description

--- crates/aoc2023/src/day03/mod.rs
+++ crates/aoc2023/src/day03/mod.rs
@@ -46,8 +46,54 @@ use common::{Problem, Solution};
 ///
 /// Of course, the actual engine schematic is much larger. *What is the sum of
 /// all of the part numbers in the engine schematic?*
+///
+/// \--- Part Two ---
+/// ----------
+///
+/// The engineer finds the missing part and installs it in the engine! As the
+/// engine springs to life, you jump in the closest gondola, finally ready to
+/// ascend to the water source.
+///
+/// You don't seem to be going very fast, though. Maybe something is still
+/// wrong? Fortunately, the gondola has a phone labeled "help", so you pick it
+/// up and the engineer answers.
+///
+/// Before you can explain the situation, she suggests that you look out the
+/// window. There stands the engineer, holding a phone in one hand and waving
+/// with the other. You're going so slowly that you haven't even left the
+/// station. You exit the gondola.
+///
+/// The missing part wasn't the only issue - one of the gears in the engine is
+/// wrong. A *gear* is any `*` symbol that is adjacent to *exactly two part
+/// numbers*. Its *gear ratio* is the result of multiplying those two numbers
+/// together.
+///
+/// This time, you need to find the gear ratio of every gear and add them all up
+/// so that the engineer can figure out which gear needs to be replaced.
+///
+/// Consider the same engine schematic again:
+///
+/// ```
+/// 467..114..
+/// ...*......
+/// ..35..633.
+/// ......#...
+/// 617*......
+/// .....+.58.
+/// ..592.....
+/// ......755.
+/// ...$.*....
+/// .664.598..
+/// ```
+///
+/// In this schematic, there are *two* gears. The first is in the top left; it
+/// has part numbers `467` and `35`, so its gear ratio is `16345`. The second
+/// gear is in the lower right; its gear ratio is `451490`. (The `*` adjacent to
+/// `617` is *not* a gear because it is only adjacent to one part number.)
+/// Adding up all of the gear ratios produces `*467835*`.
+///
+/// *What is the sum of all of the gear ratios in your engine schematic?*
 pub struct Day03;
-
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Part {
     None,
@@ -56,7 +102,6 @@ enum Part {
     Triple(u32, u32, u32),
     Quadruple(u32, u32, u32, u32),
 }
-
 impl Part {
     fn total(&self) -> u32 {
         match self {
@@ -67,14 +112,12 @@ impl Part {
             Part::Quadruple(a, b, c, d) => *a + *b + *c + *d,
         }
     }
-
     fn ratio(&self) -> u32 {
         match self {
             Part::Double(a, b) => *a * *b,
             _ => 0,
         }
     }
-
     fn add(&mut self, value: u32) {
         *self = match self {
             Part::None => Part::Single(value),
@@ -84,23 +127,17 @@ impl Part {
             Part::Quadruple(_, _, _, _) => {
                 panic!("Too many numbers in one spot");
             }
-        }
+        };
     }
 }
-
 struct PartSpan(isize, isize, isize, u32);
-
-// Fortunately, our input and enum size are small enough we can fit this entire
-// map on the stack so we can avoid heap allocations.
 struct GondolaEngine {
     parts_map: [[Part; 150]; 150],
 }
-
 impl GondolaEngine {
     fn new() -> Self {
         Self { parts_map: [[Part::None; 150]; 150] }
     }
-
     fn add_part(&mut self, span: &PartSpan) {
         for x in span.0 - 1..=span.0 + 1 {
             for y in span.1 - 1..=span.2 + 1 {
@@ -111,7 +148,6 @@ impl GondolaEngine {
             }
         }
     }
-
     fn parse(&mut self, input: &str) {
         for (idx, row) in input.lines().enumerate().map(|(i, l)| (i, l.trim())) {
             let mut span_in_use = false;
@@ -130,18 +166,15 @@ impl GondolaEngine {
                     span_in_use = false;
                 }
             }
-
             if span_in_use {
                 self.add_part(&current_span);
             }
         }
     }
-
     fn part_at(&self, x: usize, y: usize) -> Part {
         self.parts_map[x][y]
     }
 }
-
 impl Problem for Day03 {
     fn problem_input(&self) -> &'static str {
         include_str!("input.txt")
@@ -155,7 +188,6 @@ impl Problem for Day03 {
     fn solve_part1_with(&self, input: &str) -> Solution {
         let mut engine = GondolaEngine::new();
         engine.parse(input);
-
         let mut total = 0;
         for (idx, row) in input.lines().enumerate().map(|(i, l)| (i, l.trim())) {
             for (idy, ch) in row.chars().enumerate() {
@@ -164,25 +196,20 @@ impl Problem for Day03 {
                 }
             }
         }
-
         Solution::U32(total)
     }
     fn solve_part2_with(&self, input: &str) -> Solution {
         let mut engine = GondolaEngine::new();
         engine.parse(input);
-
         let mut total = 0;
-
         for (idx, row) in input.lines().enumerate().map(|(i, l)| (i, l.trim())) {
             for (idy, ch) in row.chars().enumerate() {
                 if ch != '*' {
                     continue;
                 }
-
                 total += engine.part_at(idx, idy).ratio();
             }
         }
-
         Solution::U32(total)
     }
 }
